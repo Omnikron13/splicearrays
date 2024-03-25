d57edfb92d13d6c0a7e031344b1de45e0c46280f
Joey Sabey <joey.sabey@gmx.com>
2024-03-25 16:21:37 +0000

    drop TestNode() - maybe replace wirh better tests, but idk if the coverage here is useful

--- tree/node_test.go
+++ tree/node_test.go
@@ -2,28 +2,6 @@ package tree
 
 import "testing"
 
-func TestNode(t *testing.T) {
-	bn := node{x: 0, y: 0}
-	if bn.leaf != false {
-		t.Fail()
-	}
-	if bn.x != 0 {
-		t.Fail()
-	}
-	if bn.y != 0 {
-		t.Fail()
-	}
-	ln := node{leaf: true, x: 0, y: 0}
-	if ln.leaf != true {
-		t.Fail()
-	}
-	if ln.x != 0 {
-		t.Fail()
-	}
-	if ln.y != 0 {
-		t.Fail()
-	}
-}
 func TestNodeToString(t *testing.T) {
 	t.Run("branch", func(t *testing.T) {
 		n := node{x: 1, y: 2}
